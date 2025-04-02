c51c89cacdcee286c65d471c8c3ae46e4236bd4c
Bohan Xu <unicrafter.xu@gmail.com>
2025-04-02 14:01:24 +0200

    scooter folder

---
+++ src/scooter/scooter.py
@@ -0,0 +1,22 @@
+from stmpy import Machine, Driver
+import paho.mqtt.client as mqtt
+
+broker, port = 'test.mosquitto.org', 1883
+
+
+class SCOOTER():
+    
+    def on_init(self):
+        
+        
+    def on_waiting():
+        
+        
+    def on_inUse():
+        
+        
+    def on_moving():
+        
+        
+    def on_alarm():
+        
