38061b9d396b761a41858f2fb2e807318cb27464
Bohan Xu <unicrafter.xu@gmail.com>
2025-05-01 15:37:31 +0200

    change timer

--- src/scooter/StateMachine.py
+++ src/scooter/StateMachine.py
@@ -4,7 +4,7 @@ idle = {'name': 'idle',
         'exit': 'exit_idle'}
     
 reserving = {'name': 'reserving', 
-             'entry': 'on_reserving; start_timer("t_reserving", 120000)'}
+             'entry': 'on_reserving; start_timer("t_reserving", 30000)'}
 
 alarming = {'name': 'alarming', 
             'entry': 'on_alarming', 
--- src/scooter/run.py
+++ src/scooter/run.py
@@ -5,7 +5,7 @@ from SenseHat_LED.Display import *
 scooterID = 1 # can be string of any length, increace security
 broker = "mqtt20.iik.ntnu.no" # TBD
 port = 1883 # TBD
-init_battery = 8.0
+init_battery = 8.0 # any value fron 0.0 to 8.0 with one decimal
 
 
 def run(): # with senseHat functions, only for pi with senseHat
@@ -20,30 +20,30 @@ def run(): # with senseHat functions, only for pi with senseHat
                       broker=broker, 
                       port=port, 
                       senseHat=sense1, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=8.0))
     scooter2 = Scooter(ID=2, 
                       broker=broker, 
                       port=port, 
                       senseHat=sense2, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=5.4))
     scooter3 = Scooter(ID=3, 
                       broker=broker, 
                       port=port, 
                       senseHat=sense3, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=2.2))
 
 def run_test(): # same as run(), without senseHat functions, can be tested on pc
     scooter1 = Scooter_test(ID=1, 
                       broker=broker, 
                       port=port, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=8.0))
     scooter2 = Scooter_test(ID=2, 
                       broker=broker, 
                       port=port, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=5.4))
     scooter3 = Scooter_test(ID=3, 
                       broker=broker, 
                       port=port, 
-                      battery=Battery(ID=scooterID, battery_level=init_battery))
+                      battery=Battery(ID=scooterID, battery_level=2.2))
 
 run_test()
